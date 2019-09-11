22d1429ab3182b8b0988c5c7f1312658bd768f1c
Ruairidh Wynne-McHardy <ruairidhwm@gmail.com>
2019-09-11 16:59:37 +0100

    feat: Added password length check

---
+++ packages/is/src/isPasswordLength.js
@@ -0,0 +1,6 @@
+/**
+ * Checks whether the given value has is at least 6 characters long
+ * @param {string} value
+ * @returns {boolean}
+ */
+export const isPasswordLength = (value) => value && value.length >= 6
