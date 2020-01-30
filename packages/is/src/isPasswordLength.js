eea9fbc11f7b2a1f8db93fa4af9b146c864e8f03
Ruairidh Wynne-McHardy <ruairidhwm@gmail.com>
2020-01-30 18:23:18 +0000

    👌 refactor: Switch to 8 characters

--- packages/is/src/isPasswordLength.js
+++ packages/is/src/isPasswordLength.js
@@ -1,6 +1,8 @@
 /**
- * Checks whether the given value has is at least 6 characters long
- * @param {string} value
- * @returns {boolean}
+ * @param {string} value The value to be evaluated
+ * @returns {boolean} Returns true if the value's length is greater than 8
+ *
+ * @description
+ * Checks whether the given value has is at least 8 characters long
  */
-export const isPasswordLength = (value) => value && value.length >= 6
+export const isPasswordLength = (value) => value && value.length >= 8
